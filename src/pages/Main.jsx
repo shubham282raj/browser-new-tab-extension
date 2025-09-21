7675f175b20f2738587c9d0fb6e07f243f4b389b
shubham282raj <shubham282raj@gmail.com>
2025-09-21 20:23:54 +0530

    temp rm bg idb cache

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,41 +6,39 @@ import { cacheImage, getCachedImage } from "../utils/indexedDB";
 export default function Main() {
   const [toggleSettings, setToggle] = useState(false);
 
-  const [bgUrl, setBgUrl] = useState(null);
+  const [bgUrl, setBgUrl] = useState("background.png");
 
-  useEffect(() => {
-    async function loadBackground() {
-      // get cached image
-      let url = await getCachedImage("background");
+  // useEffect(() => {
+  //   async function loadBackground() {
+  //     // get cached image
+  //     let url = await getCachedImage("background");
 
-      // if not cached, fetch and cache it
-      if (!url) {
-        const bgPath = `${import.meta.env.BASE_URL}background.png`;
-        await cacheImage(bgPath, "background");
-        url = await getCachedImage("background");
-      }
+  //     // if not cached, fetch and cache it
+  //     if (!url) {
+  //       const bgPath = `${import.meta.env.BASE_URL}background.png`;
+  //       await cacheImage(bgPath, "background");
+  //       url = await getCachedImage("background");
+  //     }
 
-      setBgUrl(url);
-    }
+  //     setBgUrl(url);
+  //   }
 
-    loadBackground();
-  }, []);
+  //   loadBackground();
+  // }, []);
 
   return (
-    bgUrl && (
-      <div
-        className="h-screen w-screen relative bg-cover bg-center"
-        style={{ backgroundImage: bgUrl ? `url(${bgUrl})` : "none" }}
-      >
-        <div className="overlayGradient"></div>
-        {toggleSettings ? (
-          <SettingsPane onClose={() => setToggle(false)} />
-        ) : (
-          <Search
-            settingsIcon={<Settings onClick={() => setToggle((v) => !v)} />}
-          />
-        )}
-      </div>
-    )
+    <div
+      className="h-screen w-screen relative bg-cover bg-center"
+      style={{ backgroundImage: bgUrl ? `url(${bgUrl})` : "none" }}
+    >
+      <div className="overlayGradient"></div>
+      {toggleSettings ? (
+        <SettingsPane onClose={() => setToggle(false)} />
+      ) : (
+        <Search
+          settingsIcon={<Settings onClick={() => setToggle((v) => !v)} />}
+        />
+      )}
+    </div>
   );
 }
