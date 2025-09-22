de3c7df7642732425e79d947ffc1d0f8eb9d957b
shubham282raj <shubham282raj@gmail.com>
2025-09-22 23:51:33 +0530

    deployment + release build conc configuration

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  // base: "/new-page/",
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "ghpages" ? "/repo-name/" : "/", // change repo-name to your GitHub repo name
+    plugins: [react(), tailwindcss()],
+  };
 });
